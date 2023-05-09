e2576f831b3e346b863b5f2c7362789f4524d09c
DDDrag0 <micheledarienzo1974@gmail.com>
2023-05-09 20:25:23 +0200

    prova con il logIn nuovo

--- URLdiMunch/src/main/java/controller/LogInServlet.java
+++ URLdiMunch/src/main/java/controller/LogInServlet.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.UserDAO;
-import model.Carrello;
 import model.User;
 
 @WebServlet("/login")
@@ -16,9 +17,11 @@ public class LogInServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private UserDAO loginDAO;
+	private UserDAO userdao;
 	
 	public void init() {
         loginDAO = new UserDAO();
+        userdao = new UserDAO();
     }
 
     public LogInServlet() {
@@ -31,29 +34,48 @@ public class LogInServlet extends HttpServlet {
 		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+        
 
+
+		
+        
+
+		
+        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		User userserv = (User)request.getSession().getAttribute("idutente");
+		if(userserv == null) {
+			userserv = new User();
+			request.getSession().setAttribute("idutente", userserv);
+		}
+		
 		String nome = request.getParameter("idutente");
         String password = request.getParameter("password");
         User user = new User();
         user.setIdUtente(nome);
         user.setPassword(password);
+        
+        String id = request.getParameter("id");
+		try {
+			userserv = userdao.ricercaUser(id);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		request.setAttribute("cart", userserv);
+		
 
         try {
+        	
+        	request.getSession().setAttribute("cart", userserv);
+    		request.setAttribute("cart", userserv);
+        	
             if (loginDAO.validate(user)) {
                 response.sendRedirect("DettagliUser.jsp");
                 
-
-
-        		Carrello cart = (Carrello)request.getSession().getAttribute("cart");
-        		if(cart == null) {
-        			cart = new Carrello();
-        			request.getSession().setAttribute("cart", cart);
-        		}
                 
-                request.getSession().setAttribute("cart", cart);
-        		request.setAttribute("cart", cart);
-        		
+       		
             } else {
                 response.sendRedirect("logIn.jsp");
             }
