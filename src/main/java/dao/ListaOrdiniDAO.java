647ea631895350aebaa73a63982f724822a55e58
DDDrag0 <micheledarienzo1974@gmail.com>
2023-06-23 15:58:10 +0200

    Modifiche alla listaOrdini.java e listaOrdiniDAO

--- src/main/java/dao/ListaOrdiniDAO.java
+++ src/main/java/dao/ListaOrdiniDAO.java
@@ -2,210 +2,272 @@ package dao;
 
 import java.security.SecureRandom;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.LinkedList;
 
 import model.ConPool;
 import model.ListaOrdini;
+import model.User;
 	
 public class ListaOrdiniDAO {
+	
+	public void insertOrder(String idProdotti, double prezzo, String indirizzo) {
+		SecureRandom rand = new SecureRandom();	//per casi di security sensitive 
+		byte[] bytes = new byte [20];
+		rand.nextBytes(bytes);
+		String co = null;
+		PreparedStatement checkcodice = null;
+	    String insertQuery = "INSERT INTO listaOrdini (idOrdine, nomeProdotto, idProdotto, prezzo, dataOrdine, indirizzoConsegna, iva) " +
+	                        "VALUES (?, ?, ?, ?, ?, ?, (SELECT iva FROM urldimunch.prodotto LIMIT 1))";
 	    
-	    public synchronized void creaOrdine(ListaOrdini listaOrdini) throws ClassNotFoundException {
+	    try (Connection connection = ConPool.getConnection();
+	         PreparedStatement statement = connection.prepareStatement(insertQuery)) {
 	    	
-			SecureRandom rand = new SecureRandom();	//per casi di security sensitive 
-			byte[] bytes = new byte [20];
-			rand.nextBytes(bytes);
-			
-			
-			String co = null;
-			PreparedStatement preparedStatement = null;
-			PreparedStatement checkcodice = null;
-	        try (Connection connection = ConPool.getConnection()){
-
-	            preparedStatement = connection.prepareStatement("INSERT INTO listaOrdini (idOrdine,nomeProdotto,prezzo,dataOrdine,dataConsegna,dataArrivo,"
-	            																+ "indirizzoConsegna, iva) VALUES (?,?,?,?,?,?,?,?);");
-	            checkcodice = connection.prepareStatement("SELECT idOrdine FROM listaOrdini where idOrdine = ?");
-	            checkcodice.setString(1, co);
-	            
-	            int ordineValido=0;
-	            int ordineInvalido=0;
-				while(ordineValido==0) {
-					int codr = rand.nextInt(999999999);
-					co= "ord-"+codr;
-					ResultSet resultSet = checkcodice.executeQuery();
-					while (resultSet.next()) {
-						String codOrdine= resultSet.getString("codice");
-						if(co.equals(codOrdine)) {
-							ordineInvalido=1;
-						}
-					}
-					if(ordineInvalido==0) {
-						ordineValido=1;
+	        //la parte del controllo per l'id ordine univoco
+	    	checkcodice = connection.prepareStatement("SELECT idOrdine FROM listaOrdini where idOrdine = ?");
+            
+            int ordineValido=0;
+            int ordineInvalido=0;
+			while(ordineValido==0) {
+				int codr = rand.nextInt(999999999);
+				co= "ord-"+codr;
+				checkcodice.setString(1, co);
+				ResultSet resultSet = checkcodice.executeQuery();
+				while (resultSet.next()) {
+					String codOrdine= resultSet.getString("codice");
+					if(co.equals(codOrdine)) {
+						ordineInvalido=1;
 					}
 				}
-				System.out.println("Il codice ordine sarà: "+co);
-	            
-	            preparedStatement.setString(1, co);
-	            //preparedStatement.setString(2, listaOrdini.getNomeProdotto());
-	            preparedStatement.setDouble(3, listaOrdini.getPrezzo());
-	            preparedStatement.setString(4, listaOrdini.getDataOrdine());
-	            preparedStatement.setString(5, listaOrdini.getDataConsegna());
-	            preparedStatement.setString(6, listaOrdini.getDataArrivo());
-	            preparedStatement.setString(7, listaOrdini.getIndirizzoConsegna());
-	            preparedStatement.setDouble(8, listaOrdini.getIva());
-	            System.out.println(preparedStatement);
-
-	        } catch (SQLException e) {
-	            printSQLException(e);
-	        }finally {
-	            try {
-	                if (preparedStatement != null) {
-	                    preparedStatement.close();
-	                }
-	                if (checkcodice != null) {
-	                    checkcodice.close();
-	                }
-	            } catch (SQLException e) {
-	                printSQLException(e);
-	            }
-	        }
-	    }
-	    
-	    public synchronized ListaOrdini ricercaOrdine(String id) throws SQLException {
-			PreparedStatement preparedStatement = null;
-
-			ListaOrdini ordine = new ListaOrdini();
-
-			String selectSQL = "SELECT * FROM listaordini WHERE idOrdine = ?";
-
-			try (Connection connection = ConPool.getConnection()){
-				preparedStatement = connection.prepareStatement(selectSQL);
-				preparedStatement.setString(1, id);
-
-				ResultSet rs = preparedStatement.executeQuery();
-
-				while (rs.next()) {
-					ordine.setIdOrdine(rs.getString("idOrdine"));
-					//ordine.setNomeProdotto(rs.getString("nomeProdotto"));
-					ordine.setPrezzo(rs.getDouble("prezzo"));
-					ordine.setDataOrdine(rs.getString("dataOrdine"));
-					ordine.setDataConsegna(rs.getString("dataConsegna"));
-					ordine.setDataArrivo(rs.getString("dataArrivo"));
-					ordine.setIndirizzoConsegna(rs.getString("indirizzoConsegna"));
-					ordine.setIva(rs.getDouble("iva"));
+				if(ordineInvalido==0) {
+					ordineValido=1;
 				}
-				preparedStatement.close();
 			}
-			catch (SQLException e) {
-	            // process sql exception
-	            printSQLException(e);
-	        }finally {
-	            try {
-	                if (preparedStatement != null) {
-	                    preparedStatement.close();
-	                }
-	            } catch (SQLException e) {
-	                printSQLException(e);
-	            }
-	        }
-			return ordine;
+			System.out.println("Il codice ordine sarà: "+co);
+	        //fine parte del controllo per l'id ordine univoco
+	    	
+	        statement.setString(1, co);
+	        
+	        String nomeProdotti = getProductNames(idProdotti);
+	        statement.setString(2, nomeProdotti);
+	        
+	        statement.setString(3, idProdotti);
+	        statement.setDouble(4, prezzo);
+	        statement.setDate(5, Date.valueOf(LocalDate.now()));
+	        statement.setString(6, indirizzo);
+	        
+	        statement.executeUpdate();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
 	    }
+	}
 
-		public synchronized boolean doDelete(String code) throws SQLException {
+	private String getProductNames(String idProdotti) {
+	    String[] nomiArray = idProdotti.split("&");
+	    StringBuilder idBuilder = new StringBuilder();
+	    
+	    try (Connection connection = ConPool.getConnection()) {
+	        for (String nome : nomiArray) {
+				PreparedStatement search = connection.prepareStatement("SELECT nome FROM urldimunch.prodotto WHERE idProdotto =  ? ");
+				search.setString(1, nome);
+				ResultSet resultSet = search.executeQuery();
+	            if (resultSet.next()) {
+	                int idProdotto = resultSet.getInt("idProdotto");
+	                idBuilder.append("ID").append(idProdotto).append("&");
+	            }
+	            resultSet.close();
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    if (idBuilder.length() > 0) {
+	        idBuilder.setLength(idBuilder.length() - 1);
+	    }
+	    return idBuilder.toString();
+	}
+
+    public synchronized ListaOrdini ricercaOrdine(String id) throws SQLException {
+		PreparedStatement preparedStatement = null;
+
+		ListaOrdini ordine = new ListaOrdini();
+
+		String selectSQL = "SELECT * FROM listaordini WHERE idOrdine = ?";
+
+		try (Connection connection = ConPool.getConnection()){
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setString(1, id);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				ordine.setIdOrdine(rs.getString("idOrdine"));
+				ordine.setNomeProdotto(rs.getString("nomeProdotto"));
+				ordine.setIdProdotto(rs.getString("idProdotto"));
+				ordine.setIdUtente(rs.getString("idUtente"));
+				ordine.setPrezzo(rs.getDouble("prezzo"));
+				ordine.setDataOrdine(rs.getString("dataOrdine"));
+				ordine.setIndirizzoConsegna(rs.getString("indirizzoConsegna"));
+				ordine.setIva(rs.getDouble("iva"));
+			}
+			preparedStatement.close();
+		}
+		catch (SQLException e) {
+            // process sql exception
+            printSQLException(e);
+        }finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                printSQLException(e);
+            }
+        }
+		return ordine;
+    }
+
+	public synchronized boolean doDelete(String code) throws SQLException {
+		
+		PreparedStatement preparedStatement = null;
+
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM listaOrdini WHERE idOrdine = ?";
+
+		try (Connection connection = ConPool.getConnection()){
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setString(1, code);
+
+			result = preparedStatement.executeUpdate();
 			
-			PreparedStatement preparedStatement = null;
+			preparedStatement.close();
 
-			int result = 0;
+		}
+		catch (SQLException e) {
+            // process sql exception
+            printSQLException(e);
+        }finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                printSQLException(e);
+            }
+        }
+		return (result != 0);
+	}
 
-			String deleteSQL = "DELETE FROM listaOrdini WHERE idOrdine = ?";
+	public synchronized Collection<ListaOrdini> ricercaTuttiOrdini() throws SQLException {
+		
+		PreparedStatement preparedStatement = null;
 
-			try (Connection connection = ConPool.getConnection()){
-				preparedStatement = connection.prepareStatement(deleteSQL);
-				preparedStatement.setString(1, code);
+		Collection<ListaOrdini> orders = new LinkedList<>();
 
-				result = preparedStatement.executeUpdate();
+		String selectSQL = "SELECT * FROM  listaOrdini";
+
+		try (Connection connection = ConPool.getConnection()){
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				ListaOrdini ordine = new ListaOrdini();
+
+				ordine.setIdOrdine(rs.getString("idOrdine"));
+				ordine.setNomeProdotto(rs.getString("nomeProdotto"));
+				ordine.setIdProdotto(rs.getString("idProdotto"));
+				ordine.setIdUtente(rs.getString("idUtente"));
+				ordine.setPrezzo(rs.getDouble("prezzo"));
+				ordine.setDataOrdine(rs.getString("dataOrdine"));
+				ordine.setIndirizzoConsegna(rs.getString("indirizzoConsegna"));
+				ordine.setIva(rs.getDouble("iva"));
 				
-				preparedStatement.close();
-
+				orders.add(ordine);
 			}
-			catch (SQLException e) {
-	            // process sql exception
-	            printSQLException(e);
-	        }finally {
-	            try {
-	                if (preparedStatement != null) {
-	                    preparedStatement.close();
-	                }
-	            } catch (SQLException e) {
-	                printSQLException(e);
-	            }
-	        }
-			return (result != 0);
-		}
+			preparedStatement.close();
 
-		public synchronized Collection<ListaOrdini> ricercaTuttiOrdini() throws SQLException {
+		}
+		catch (SQLException e) {
+            // process sql exception
+            printSQLException(e);
+        }finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                printSQLException(e);
+            }
+        }
+		return orders;
+	}
+	
+public synchronized Collection<ListaOrdini> ricercaTuttiOrdiniUtente(User user) throws SQLException {
+		
+		PreparedStatement preparedStatement = null;
+
+		Collection<ListaOrdini> orders = new LinkedList<>();
+		
+
+		try (Connection connection = ConPool.getConnection()){
 			
-			PreparedStatement preparedStatement = null;
+			PreparedStatement search = connection.prepareStatement("SELECT * FROM  listaOrdini WHERE idUtente= ? ");
+			search.setString(1, user.getIdUtente());
+			ResultSet rs = search.executeQuery();
 
-			Collection<ListaOrdini> orders = new LinkedList<>();
+			while (rs.next()) {
+				ListaOrdini ordine = new ListaOrdini();
 
-			String selectSQL = "SELECT * FROM  listaOrdini";
-
-			try (Connection connection = ConPool.getConnection()){
-				preparedStatement = connection.prepareStatement(selectSQL);
-
-				ResultSet rs = preparedStatement.executeQuery();
-
-				while (rs.next()) {
-					ListaOrdini ordine = new ListaOrdini();
-
-					ordine.setIdOrdine(rs.getString("idOrdine"));
-					//ordine.setIdProdotto(rs.getString("nomeProdotto"));
-					ordine.setPrezzo(rs.getDouble("prezzo"));
-					ordine.setDataOrdine(rs.getString("dataOrdine"));
-					ordine.setDataConsegna(rs.getString("dataConsegna"));
-					ordine.setDataArrivo(rs.getString("dataArrivo"));
-					ordine.setIndirizzoConsegna(rs.getString("indirizzoConsegna"));
-					ordine.setIva(rs.getDouble("iva"));
-					
-					orders.add(ordine);
-				}
-				preparedStatement.close();
-
+				ordine.setIdOrdine(rs.getString("idOrdine"));
+				ordine.setNomeProdotto(rs.getString("nomeProdotto"));
+				ordine.setIdProdotto(rs.getString("idProdotto"));
+				ordine.setIdUtente(rs.getString("idUtente"));
+				ordine.setPrezzo(rs.getDouble("prezzo"));
+				ordine.setDataOrdine(rs.getString("dataOrdine"));
+				ordine.setIndirizzoConsegna(rs.getString("indirizzoConsegna"));
+				ordine.setIva(rs.getDouble("iva"));
+				
+				orders.add(ordine);
 			}
-			catch (SQLException e) {
-	            // process sql exception
-	            printSQLException(e);
-	        }finally {
-	            try {
-	                if (preparedStatement != null) {
-	                    preparedStatement.close();
-	                }
-	            } catch (SQLException e) {
-	                printSQLException(e);
-	            }
-	        }
-			return orders;
-		}
-	    
+			search.close();
 
-	    private void printSQLException(SQLException ex) {
-	        for (Throwable e: ex) {
-	            if (e instanceof SQLException) {
-	                e.printStackTrace(System.err);
-	                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
-	                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
-	                System.err.println("Message: " + e.getMessage());
-	                Throwable t = ex.getCause();
-	                while (t != null) {
-	                    System.out.println("Cause: " + t);
-	                    t = t.getCause();
-	                }
-	            }
-	        }
-	    }
+		}
+		catch (SQLException e) {
+            // process sql exception
+            printSQLException(e);
+        }finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                printSQLException(e);
+            }
+        }
+		return orders;
+	}
+    
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
 
 }
--- src/main/java/model/ListaOrdini.java
+++ src/main/java/model/ListaOrdini.java
@@ -5,31 +5,36 @@ import java.util.List;
 public class ListaOrdini {
 
 	private String idOrdine;
-	private List<String> nomeProdotto;
-	private List<String> idProdotto;
+	private String nomeProdotto;
+	private String idProdotto;
+	private String idUtente;
 	private String indirizzoConsegna;
 	private double prezzo;
 	private String dataOrdine;
-	private String dataConsegna;
-	private String dataArrivo;
 	private double iva;
 	
+	public String getIdUtente() {
+		return idUtente;
+	}
+	public void setIdUtente(String idUtente) {
+		this.idUtente = idUtente;
+	}
 	public String getIdOrdine() {
 		return idOrdine;
 	}
 	public void setIdOrdine(String idOrdine) {
 		this.idOrdine = idOrdine;
 	}
-	public List<String> getNomeProdotto() {
+	public String getNomeProdotto() {
 		return nomeProdotto;
 	}
-	public void setNomeProdotto(List<String> nomeProdotto) {
+	public void setNomeProdotto(String nomeProdotto) {
 		this.nomeProdotto = nomeProdotto;
 	}
-	public List<String> getIdProdotto() {
+	public String getIdProdotto() {
 		return idProdotto;
 	}
-	public void setIdProdotto(List<String> idProdotto) {
+	public void setIdProdotto(String idProdotto) {
 		this.idProdotto = idProdotto;
 	}
 	public String getIndirizzoConsegna() {
@@ -50,18 +55,6 @@ public class ListaOrdini {
 	public void setDataOrdine(String dataOrdine) {
 		this.dataOrdine = dataOrdine;
 	}
-	public String getDataConsegna() {
-		return dataConsegna;
-	}
-	public void setDataConsegna(String dataConsegna) {
-		this.dataConsegna = dataConsegna;
-	}
-	public String getDataArrivo() {
-		return dataArrivo;
-	}
-	public void setDataArrivo(String dataArrivo) {
-		this.dataArrivo = dataArrivo;
-	}
 	public double getIva() {
 		return iva;
 	}
@@ -72,6 +65,6 @@ public class ListaOrdini {
 	public String toString() {
 		return "ListaOrdini [idOrdine=" + idOrdine + ", nomeProdotto=" + nomeProdotto 
 				+ ", indirizzoConsegna=" + indirizzoConsegna + ", prezzo=" + prezzo + ", dataOrdine=" + dataOrdine 
-				+ ", dataConsegna=" + dataConsegna + ", dataArrivo=" + dataArrivo + ", iva=" + iva + "]";
+				+ ", dataConsegna=" + ", dataArrivo=" + ", iva=" + iva + "]";
 	}
 }
