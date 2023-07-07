caa1b70aa8cedb7d1bcb7e8c7eeeec07a6572bce
DDDrag0 <micheledarienzo1974@gmail.com>
2023-07-07 02:47:07 +0200

    aggiunto il db nuovo

--- build/db/db finale.sql
+++ build/db/db finale.sql
@@ -18,30 +18,6 @@ USE `urldimunch`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `imgordini`
---
-
-DROP TABLE IF EXISTS `imgordini`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `imgordini` (
-  `idimgOrdini` int NOT NULL,
-  `idOrdine` varchar(45) DEFAULT NULL,
-  `image` mediumblob,
-  PRIMARY KEY (`idimgOrdini`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `imgordini`
---
-
-LOCK TABLES `imgordini` WRITE;
-/*!40000 ALTER TABLE `imgordini` DISABLE KEYS */;
-/*!40000 ALTER TABLE `imgordini` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `listaordini`
 --
 
@@ -52,7 +28,8 @@ CREATE TABLE `listaordini` (
   `idOrdine` varchar(45) NOT NULL,
   `nomeProdotto` varchar(1000) NOT NULL,
   `idProdotto` varchar(1000) NOT NULL,
-  `quantità` varchar(1000) DEFAULT NULL,
+  `quantita` varchar(1000) NOT NULL,
+  `imagepath` varchar(10000) NOT NULL,
   `idUtente` varchar(45) NOT NULL,
   `prezzo` double NOT NULL,
   `dataOrdine` date NOT NULL,
@@ -68,7 +45,7 @@ CREATE TABLE `listaordini` (
 
 LOCK TABLES `listaordini` WRITE;
 /*!40000 ALTER TABLE `listaordini` DISABLE KEYS */;
-INSERT INTO `listaordini` VALUES ('ord-157027494','Notte Stellata','3_Notte',NULL,'admin',259,'2023-07-03','LucaAbate, 32',22),('ord-199015920','Notte Stellata','3_Notte',NULL,'admin',259,'2023-07-03','LucaAbate, 32',22),('ord-230806768','Notte Stellata&Venere','3_Notte&1_Venere',NULL,'a',503,'2023-07-01','empty',22),('ord-251251205','Notte Stellata','3_Notte',NULL,'Michele',259,'2023-07-02','empty',22),('ord-308215366','Notte Stellata','3_Notte',NULL,'admin',747,'2023-07-03','LucaAbate, 32',22),('ord-36130111','Notte Stellata','3_Notte',NULL,'admin',259,'2023-07-03','LucaAbate, 32',22),('ord-386615235','Notte Stellata&Venere&Guernica','3_Notte&1_Venere&4_Guernica',NULL,'admin',706.74,'2023-07-03','LucaAbate, 32',22),('ord-42938973','Notte Stellata&Venere','3_Notte&1_Venere',NULL,'admin',503,'2023-07-03','LucaAbate, 32',22),('ord-468669397','Notte Stellata&aaaa','3_Notte&aaaa',NULL,'admin',529.84,'2023-07-03','LucaAbate, 32',22),('ord-480390573','Venere&Guernica','1_Venere&4_Guernica',NULL,'Michele',462.74,'2023-07-02','empty',22),('ord-584815311','Venere&Guernica','1_Venere&4_Guernica',NULL,'admin',462.74,'2023-07-03','LucaAbate, 32',22),('ord-598481604','pazzo&Notte Stellata','pazzo&3_Notte',NULL,'a',261.44,'2023-07-01','empty',22),('ord-7028136','Venere&Notte Stellata&aaaa','1_Venere&3_Notte&aaaa',NULL,'admin',773.84,'2023-07-03','LucaAbate, 32',22),('ord-820396432','Notte Stellata','3_Notte',NULL,'3_Notte',259,'2023-06-28','empty',22),('ord-862062904','Venere&Guernica','1_Venere&4_Guernica',NULL,'admin',462.74,'2023-07-03','LucaAbate, 32',22),('ord-868988559','Notte Stellata&Venere&Guernica','3_Notte&1_Venere&4_Guernica',NULL,'admin',706.74,'2023-07-03','LucaAbate, 32',22),('ord-907500005','Venere&Guernica','1_Venere&4_Guernica',NULL,'admin',462.74,'2023-07-03','LucaAbate, 32',22),('ord-918283182','Notte Stellata&Venere','3_Notte&1_Venere',NULL,'3_Notte&1_Venere',503,'2023-07-01','empty',22),('ord-971922035','Notte Stellata&Venere','3_Notte&1_Venere',NULL,'admin',503,'2023-07-03','LucaAbate, 32',22),('ord-97529639','Venere&Guernica','1_Venere&4_Guernica',NULL,'admin',462.74,'2023-07-03','LucaAbate, 32',22),('ord-983697640','Notte Stellata&aaaa','3_Notte&aaaa',NULL,'3_Notte&aaaa',773.84,'2023-06-28','empty',22);
+INSERT INTO `listaordini` VALUES ('ord-587540054','Notte Stellata&Guernica','3_Notte&4_Guernica','3&1','/image/notte.jpg&/image/guernica.jpg','admin',950.74,'2023-07-07','LucaAbate, 32',22);
 /*!40000 ALTER TABLE `listaordini` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,8 +58,8 @@ DROP TABLE IF EXISTS `prodotto`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `prodotto` (
   `idProdotto` varchar(45) NOT NULL,
-  `nome` varchar(45) NOT NULL,
-  `artista` varchar(45) DEFAULT NULL,
+  `nome` varchar(1000) NOT NULL,
+  `artista` varchar(1000) DEFAULT NULL,
   `tipo` varchar(45) DEFAULT NULL,
   `epoca` varchar(45) DEFAULT NULL,
   `dimensioni` varchar(45) DEFAULT NULL,
@@ -90,8 +67,7 @@ CREATE TABLE `prodotto` (
   `quantità` int DEFAULT NULL,
   `iva` double DEFAULT NULL,
   `prezzo` double DEFAULT NULL,
-  `imagepath` varchar(100) DEFAULT NULL,
-  `immagine` mediumblob,
+  `imagepath` varchar(1000) DEFAULT NULL,
   `dataaggiunta` date DEFAULT NULL,
   PRIMARY KEY (`idProdotto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -103,7 +79,7 @@ CREATE TABLE `prodotto` (
 
 LOCK TABLES `prodotto` WRITE;
 /*!40000 ALTER TABLE `prodotto` DISABLE KEYS */;
-INSERT INTO `prodotto` VALUES ('1_Venere','Venere','Botticelli','Quadro','Vecchia','20*20','Venere dipinta da Botticelli',984,22,200,'/image/venere.jpg',NULL,NULL),('2_Urlo','Urlo di Munch','Munch','Quadro','Vecchia','30*30','L\'urlo di Munch dipinto dall\'Omonimo',999,22,450,'/image/urlo.jpg',NULL,NULL),('3_Notte','Notte Stellata','Van Gogh','Quadro','Vecchia','20*20','Notte stellata di Van Gogh',-4369,22,200,'/image/notte.jpg',NULL,NULL),('4_Guernica','Guernica','Picasso','Quadro','Vecchia','20*20','Guernica di Picasso',989,22,167,'/image/guernica.jpg',NULL,NULL),('5_Gioconda','Gioconda','Lenoardo Da Vinci','Quadro','Vecchia','30*30','La famosa Gioconda di Leonardo da Vinci',999,22,432,'/image/gioconda.png',NULL,NULL),('aaaa','aaaa','a','a','a','aa','a',996,22,222,NULL,NULL,NULL),('b','b','b','b','b','b','b',34,33,223,NULL,NULL,NULL),('pazzo','pazzo','veramente pazzo','pazzissimo','pazza','pazze','pazza descrizione',11,22,2,NULL,NULL,NULL);
+INSERT INTO `prodotto` VALUES ('1_Venere','Venere','Botticelli','Quadro','Vecchia','20*20','Venere dipinta da Botticelli',984,22,200,'/image/venere.jpg',NULL),('2_Urlo','Urlo di Munch','Munch','Quadro','Vecchia','30*30','L\'urlo di Munch dipinto dall\'Omonimo',999,22,450,'/image/urlo.jpg',NULL),('3_Notte','Notte Stellata','Van Gogh','Quadro','Vecchia','20*20','Notte stellata di Van Gogh',-4372,22,200,'/image/notte.jpg',NULL),('4_Guernica','Guernica','Picasso','Quadro','Vecchia','20*20','Guernica di Picasso',988,22,167,'/image/guernica.jpg',NULL),('5_Gioconda','Gioconda','Lenoardo Da Vinci','Quadro','Vecchia','30*30','La famosa Gioconda di Leonardo da Vinci',999,22,432,'/image/gioconda.png',NULL),('prod-20560','Composition with Red, Blue, and Yellow','Piet Mondrian','Painting','1900-2000','40cm x 40cm','Questo quadro astratto rappresenta l\'essenza della pittura neoplasticista di Mondrian. Caratterizzato da linee rette, forme geometriche e l\'uso di colori primari, è considerato un\'icona del movimento De Stijl.',543,22,90,'/image/Piet_Mondriaan,_1930_-_Mondrian_Composition_II_in_Red,_Blue,_and_Yellow.jpg','2023-07-07'),('prod-30078','Nighthawks','Edward Hopper','Painting','before 1500','70cm x 100cm','Questo dipinto realista americano ritrae un gruppo di persone solitarie in un locale notturno. La composizione suggestiva, l\'uso del colore e l\'atmosfera di solitudine hanno reso questo quadro un\'icona dell\'arte moderna.',232,22,134,'/image/Hopper_Nighthawks.jpg','2023-07-07'),('prod-59546','Il giardino delle delizie','Hieronymus Bosch','Painting','1500-1600','100cm x 70cm','Questo dipinto surrealista del Rinascimento olandese è noto per la sua complessità e il suo simbolismo enigmatico. Raffigura una scena paradisiaca nella parte sinistra, una scena di peccato e desiderio al centro e una visione apocalittica dell\'inferno nella parte destra.',322,22,323,'/image/El_jardín_de_las_Delicias,_de_El_Bosco.jpg','2023-07-07'),('prod-82604','Persistenza della Memoria','Salvador Dali','Painting','before 1500','40cm x 40cm','\r\nLa Persistenza della Memoria è un celebre dipinto di Salvador Dalí creato nel 1931. È un\'opera surrealista che rappresenta un paesaggio desertico con orologi molli e deformati. Gli orologi pendono da rami di alberi morti o giacciono sul terreno, creando un senso di tempo fluido e instabile. Il quadro è dipinto ad olio su tela e misura 24 x 33 cm. È custodito nel Museum of Modern Art di New York. La scena si svolge in un ambiente lunare con un mare tranquillo sullo sfondo. Questa immagine surreale sfida la nostra percezione convenzionale del tempo. La Persistenza della Memoria è diventata un\'icona dell\'arte surrealista, simbolo dell\'interesse di Dalí per l\'inconscio e la psicoanalisi. L\'opera invita a esplorare le profondità dell\'inconscio e a riflettere sulla relatività del tempo e della realtà.',233,22,75,'/image/salvador-dali-persistenza-memoria.jpg','2023-07-06');
 /*!40000 ALTER TABLE `prodotto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -129,6 +105,7 @@ CREATE TABLE `recensione` (
 
 LOCK TABLES `recensione` WRITE;
 /*!40000 ALTER TABLE `recensione` DISABLE KEYS */;
+INSERT INTO `recensione` VALUES (2321321,'2_Urlo','Francuccio','veramente bello'),(3213213,'2_Urlo','Felice','sono felice'),(64322721,'2_Urlo','admin','spettacolare sto quadro!'),(312321313,'2_Urlo','Lucariello','Ottimo servizio'),(778647919,'1_Venere','admin','un quadro eccezzionale!');
 /*!40000 ALTER TABLE `recensione` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +137,7 @@ CREATE TABLE `utente` (
 
 LOCK TABLES `utente` WRITE;
 /*!40000 ALTER TABLE `utente` DISABLE KEYS */;
-INSERT INTO `utente` VALUES ('admin','Ab123456','SonoIO','Parito','lucaabate@csa.com','xxxxxxxxxxxx1111','333 222 1112','LucaAbate','LucaAbate, 32',1);
+INSERT INTO `utente` VALUES ('admin','Ab123456','SonoIO','Parito','lucaabate@csa.com','xxxxxxxxxxxx1111','333 222 1112','LucaAbate','LucaAbate, 32',1),('banana','Banana123','empty','empty','banana@s','empty','empty','empty','empty',0),('Ciao','Hsasd232','empty','empty','sicaio@ahhaha','empty','empty','empty','empty',0),('dsadsadas','dsdadsadas2A','empty','empty','Adasdsa@asd','empty','empty','empty','empty',0);
 /*!40000 ALTER TABLE `utente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -181,4 +158,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-05  2:55:23
+-- Dump completed on 2023-07-07  2:46:15
