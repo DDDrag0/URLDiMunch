14af69c48dce961878ac04a56619d8401d82e3d4
DDDrag0 <micheledarienzo1974@gmail.com>
2023-07-08 14:05:48 +0200

    [SC] riprovo a rebuildare tutto

--- src/main/java/dao/ProdottoDAO.java
+++ src/main/java/dao/ProdottoDAO.java
@@ -30,85 +30,82 @@ public class ProdottoDAO {
 	String data ="dataaggiunta";
 	
 	public synchronized int doSave(Prodotto product) throws SQLException {
-	    SecureRandom rand = new SecureRandom();
-	    byte[] bytes = new byte[20];
-	    rand.nextBytes(bytes);
-	    String co = null;
-	    PreparedStatement checkcodice = null;
-	    PreparedStatement preparedStatement = null;
-	    PreparedStatement subqueryStatement = null;
+		SecureRandom rand = new SecureRandom();	//per casi di security sensitive 
+		byte[] bytes = new byte [20];
+		rand.nextBytes(bytes);
+		String co = null;
+		PreparedStatement checkcodice = null;
+		PreparedStatement preparedStatement = null;
+
 	    double iva = 0.0;
-	    int result = 0;
-	    String subquery = "SELECT iva FROM urldimunch.prodotto LIMIT 1";
-	    String insertSQL = "INSERT INTO prodotto (idProdotto, nome, artista, tipo, epoca, dimensioni, descrizione, quantità, iva, prezzo, dataaggiunta, imagepath) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
-	    try (Connection connection2 = ConPool.getConnection();
-	         Connection connection = ConPool.getConnection()) {
+		int result = 0;
+		
+		String subquery = "SELECT iva FROM urldimunch.prodotto LIMIT 1";
+		String insertSQL = "INSERT INTO prodotto (idProdotto,nome, artista, tipo, epoca, dimensioni, descrizione, quantità, iva, prezzo, dataaggiunta, imagepath) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
 
-	        // Esegue la subquery per ottenere il valore di iva
-	        subqueryStatement = connection2.prepareStatement(subquery);
-	        ResultSet subqueryResult = subqueryStatement.executeQuery();
-	        if (subqueryResult.next()) {
-	            iva = subqueryResult.getDouble("iva");
-	        }
+		try (Connection connection2 = ConPool.getConnection()) {
+		    // Esegui la subquery per ottenere il valore di iva
+		    PreparedStatement subqueryStatement = connection2.prepareStatement(subquery);
+		    ResultSet subqueryResult = subqueryStatement.executeQuery();
+		    if (subqueryResult.next()) {
+		        iva = subqueryResult.getDouble("iva");
+		    }
+		}
+		
+		try (Connection connection = ConPool.getConnection()){
 
-	        // Parte del controllo per l'id prodotto univoco
-	        checkcodice = connection.prepareStatement("SELECT idProdotto FROM prodotto WHERE idProdotto = ?");
-	        int ordineValido = 0;
-	        int ordineInvalido = 0;
-	        while (ordineValido == 0) {
-	            int codr = rand.nextInt(99999);
-	            co = "prod-" + codr;
-	            checkcodice.setString(1, co);
-	            ResultSet resultSet = checkcodice.executeQuery();
-	            while (resultSet.next()) {
-	                String codOrdine = resultSet.getString("idProdotto");
-	                if (co.equals(codOrdine)) {
-	                    ordineInvalido = 1;
-	                }
-	            }
-	            if (ordineInvalido == 0) {
-	                ordineValido = 1;
-	            }
-	        }
-	        // Fine parte del controllo per l'id prodotto univoco
+			//la parte del controllo per l'id prodotto univoco
+	    	checkcodice = connection.prepareStatement("SELECT idProdotto FROM prodotto where idProdotto = ?");
+	        
+	        int ordineValido=0;
+	        int ordineInvalido=0;
+			while(ordineValido==0) {
+				int codr = rand.nextInt(99999);
+				co= "prod-"+codr;
+				checkcodice.setString(1, co);
+				ResultSet resultSet = checkcodice.executeQuery();
+				while (resultSet.next()) {
+					String codOrdine= resultSet.getString("codice");
+					if(co.equals(codOrdine)) {
+						ordineInvalido=1;
+					}
+				}
+				if(ordineInvalido==0) {
+					ordineValido=1;
+				}
+			}
+	        //fine parte del controllo per l'id prodotto univoco
+			
+			preparedStatement = connection.prepareStatement(insertSQL);
+			preparedStatement.setString(1, co);
+			preparedStatement.setString(2, product.getNome());
+			preparedStatement.setString(3, product.getArtista());
+			preparedStatement.setString(4, product.getTipo());
+			preparedStatement.setString(5, product.getEpoca());
+			preparedStatement.setString(6, product.getDimensioni());
+			preparedStatement.setString(7, product.getDescrizione());
+			preparedStatement.setInt(8, product.getQuantita());
+		    preparedStatement.setDouble(9, iva);
+		    preparedStatement.setDouble(10, product.getPrezzo());
+		    preparedStatement.setDate(11, Date.valueOf(LocalDate.now()));
+		    preparedStatement.setString(12, product.getImagepath());
 
-	        // Esegue l'inserimento del prodotto nel database
-	        preparedStatement = connection.prepareStatement(insertSQL);
-	        preparedStatement.setString(1, co);
-	        preparedStatement.setString(2, product.getNome());
-	        preparedStatement.setString(3, product.getArtista());
-	        preparedStatement.setString(4, product.getTipo());
-	        preparedStatement.setString(5, product.getEpoca());
-	        preparedStatement.setString(6, product.getDimensioni());
-	        preparedStatement.setString(7, product.getDescrizione());
-	        preparedStatement.setInt(8, product.getQuantita());
-	        preparedStatement.setDouble(9, iva);
-	        preparedStatement.setDouble(10, product.getPrezzo());
-	        preparedStatement.setDate(11, Date.valueOf(LocalDate.now()));
-	        preparedStatement.setString(12, product.getImagepath());
-	        result = preparedStatement.executeUpdate();
-	    } catch (SQLException e) {
-	        // Processa l'eccezione SQL
-	        printSQLException(e);
-	    } finally {
-	        try {
-	            if (preparedStatement != null) {
-	                preparedStatement.close();
-	            }
-	            if (checkcodice != null) {
-	                checkcodice.close();
-	            }
-	            if (subqueryStatement != null) {
-	                subqueryStatement.close();
-	            }
-	        } catch (SQLException e) {
-	            // Gestisci o registra l'eccezione
-	            printSQLException(e);
-	        }
-	    }
-	    return result;
-	}
+            result = preparedStatement.executeUpdate();
 
+        } catch (SQLException e) {
+            // process sql exception
+        	//printSQLException(e); //Sensitive
+        }finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+            	//printSQLException(e); //Sensitive
+            }
+        }
+        return result;
+    }
 
 	public synchronized Prodotto doRetrieveByKey(String code) throws SQLException {
 		PreparedStatement preparedStatement = null;
@@ -141,14 +138,14 @@ public class ProdottoDAO {
 		}
 		catch (SQLException e) {
             // process sql exception
-            printSQLException(e);
+        	//printSQLException(e); //Sensitive
         }finally {
             try {
                 if (preparedStatement != null) {
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
-                printSQLException(e);
+            	//printSQLException(e); //Sensitive
             }
         }
 		return prodotto;
@@ -188,14 +185,14 @@ public class ProdottoDAO {
 		}
 		catch (SQLException e) {
             // process sql exception
-            printSQLException(e);
+        	//printSQLException(e); //Sensitive
         }finally {
             try {
                 if (preparedStatement != null) {
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
-                printSQLException(e);
+            	//printSQLException(e); //Sensitive
             }
         }
 		return a;
@@ -221,14 +218,14 @@ public class ProdottoDAO {
 		}
 		catch (SQLException e) {
             // process sql exception
-            printSQLException(e);
+        	//printSQLException(e); //Sensitive
         }finally {
             try {
                 if (preparedStatement != null) {
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
-                printSQLException(e);
+            	//printSQLException(e); //Sensitive
             }
         }
 		return (result != 0);
@@ -253,14 +250,14 @@ public class ProdottoDAO {
 		}
 		catch (SQLException e) {
             // process sql exception
-            printSQLException(e);
+        	//printSQLException(e); //Sensitive
         }finally {
             try {
                 if (preparedStatement != null) {
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
-                printSQLException(e);
+            	//printSQLException(e); //Sensitive
             }
         }
 		return (result != 0);
@@ -283,14 +280,14 @@ public class ProdottoDAO {
 		}
 		catch (SQLException e) {
             // process sql exception
-            printSQLException(e);
+        	//printSQLException(e); //Sensitive
         }finally {
             try {
                 if (preparedStatement != null) {
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
-                printSQLException(e);
+            	//printSQLException(e); //Sensitive
             }
         }
 		return (result != 0);
@@ -311,7 +308,7 @@ public class ProdottoDAO {
 	        	
 	        }
 	    } catch (SQLException e) {
-	        printSQLException(e);
+        	//printSQLException(e); //Sensitive
 	        throw e;
 	    }
 	}
@@ -351,31 +348,31 @@ public class ProdottoDAO {
 		}
 		catch (SQLException e) {
             // process sql exception
-            printSQLException(e);
+        	//printSQLException(e); //Sensitive
         }finally {
             try {
                 if (preparedStatement != null) {
                     preparedStatement.close();
                 }
             } catch (SQLException e) {
-                printSQLException(e);
+            	//printSQLException(e); //Sensitive
             }
         }
 		return products;
 	}
 	
-    private void printSQLException(SQLException ex) {
+	private void printSQLException(SQLException ex) {
         for (Throwable e: ex) {
             if (e instanceof SQLException) {
-                //e.printStackTrace(//System.err);
+                //e.printStackTrace(System.err);
                 //System.err.println("SQLState: " + ((SQLException) e).getSQLState());
                 //System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
                 //System.err.println("Message: " + e.getMessage());
-                //Throwable t = ex.getCause();
-                //while (t != null) {
-                    //System.out.println("Cause: " + t);
-                    //t = t.getCause();
-                //}
+            	//Throwable t = ex.getCause();
+            	//while (t != null) {
+            	//    System.out.println("Cause: " + t);
+            	//    t = t.getCause();
+            	//}
             }
         }
     }
